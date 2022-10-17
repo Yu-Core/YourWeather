3873525b2dce937552f50d4a5f76043355819d0a
Yu-Core <310513125@qq.com>
2022-10-18 00:19:48 +0800

    添加项目文件。

---
+++ YourWeather/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace YourWeather;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ YourWeather/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace YourWeather;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
