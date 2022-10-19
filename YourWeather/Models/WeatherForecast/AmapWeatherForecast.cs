352a2fd4bd2889e3f7898b4dbef51b3b6cefd84b
Yu-Core <310513125@qq.com>
2022-10-19 23:51:46 +0800

    加载动画

---
+++ YourWeather/Models/WeatherForecast/AmapWeatherForecast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourWeather.Models.WeatherForecast
+{
+    public class AmapWeatherForecast
+    {
+        public string Status { get; set; }
+        public string Count { get; set; }
+        public string Info { get; set; }
+        public string Infocode { get; set; }
+        public Forecast[] Forecasts { get; set; }
+    }
+
+    public class Forecast
+    {
+        public string City { get; set; }
+        public string Adcode { get; set; }
+        public string Province { get; set; }
+        public string Reporttime { get; set; }
+        public Cast[] Casts { get; set; }
+    }
+
+    public class Cast
+    {
+        public string Date { get; set; }
+        public string Week { get; set; }
+        public string Dayweather { get; set; }
+        public string Nightweather { get; set; }
+        public string Daytemp { get; set; }
+        public string Nighttemp { get; set; }
+        public string Daywind { get; set; }
+        public string Nightwind { get; set; }
+        public string Daypower { get; set; }
+        public string Nightpower { get; set; }
+    }
+
+}
